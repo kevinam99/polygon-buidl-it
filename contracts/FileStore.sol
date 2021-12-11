e6e79d877e5decb0f407f33eeb7b59afca4e662f
Kevin Mathew <kevinam99@gmail.com>
2021-12-11 23:13:50 +0530

    Share files

--- contracts/FileStore.sol
+++ contracts/FileStore.sol
@@ -1,35 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract FileStore {
   address admin;
   bool active = true;
 
-  mapping (address => string) ownerToFile; 
+  mapping (address => string[]) public ownerToFiles; 
+  mapping (string => address[]) public fileToOwners;
+  mapping (address => mapping(address => string[])) public sharedFiles;
+  
   constructor() public {
     admin = msg.sender;
   }
 
   modifier isActive {
-    require(active == true);
+    require(active == true, "Service inactive");
     _;
   }
 
   modifier onlyOwner {
-    require(msg.sender == admin);
+    require(msg.sender == admin, "This user has no access");
     _;
   }
 
-  function addFile(string memory _cid) public isActive returns (string memory){
-    ownerToFile[msg.sender] = _cid;
-    return ownerToFile[msg.sender];
+  function addFile(string memory _cid) public isActive returns (string[] memory){
+    ownerToFiles[msg.sender].push(_cid);
+    fileToOwners[_cid].push(msg.sender);
+    return ownerToFiles[msg.sender];
   }
 
-  function getFile() public view returns (string memory) {
-    return ownerToFile[msg.sender];
+  function getFile(string memory _cid) public isActive view returns (string memory) {
+    string[] memory files = getFiles();
+    for(uint i = 0; i < files.length; i++) {
+      if(keccak256(abi.encodePacked(files[i])) == keccak256(abi.encodePacked(_cid))) {
+        return files[i];
+      }
+    }
+  }
+
+  function getFiles() public isActive view returns (string[] memory) {
+    return ownerToFiles[msg.sender];
   }
 
   function toggleServiceStatus() public onlyOwner {
     active = !active;
+  }
+
+  function getFilesFor(address _for) public isActive view returns (string[] memory) {
+    return ownerToFiles[_for];
+  }
+
+  function shareFile(address _with, string memory _cid) public isActive returns (string[] memory) {
+    require(keccak256(abi.encodePacked(getFile(_cid))) == keccak256(abi.encodePacked(_cid)));
+
+    sharedFiles[msg.sender][_with].push(_cid);
+    ownerToFiles[_with].push(_cid);
+    fileToOwners[_cid].push(_with);
+    return sharedFiles[msg.sender][_with];
+    
   }
 }
--- test/file_store.js
+++ test/file_store.js
@@ -1,8 +1,8 @@
 const FileStore = artifacts.require("./FileStore.sol")
-let tokenInstance;
+let contractInstance;
 (async () =>{
     try {
-        tokenInstance = await FileStore.deployed()
+        contractInstance = await FileStore.deployed()
     }
     catch(error) {
         console.error(error)
@@ -10,17 +10,31 @@ let tokenInstance;
 })()
 
 contract("FileStore", accounts => {
-    it("initialises the contract with the correct values", async() =>{
+    it("stores the files with the correct cid", async() =>{
         try {
           const _cid = "some test cid"
-            const addFile = await tokenInstance.addFile.call(_cid)
-            assert.equal(addFile, _cid, "has the correct cid")
-            const getFile = await tokenInstance.getFile()
-            assert.equal(getFile, _cid, "has the correct cid")
+          const addFile = await contractInstance.addFile.call(_cid)
+          assert.equal(addFile, _cid, "has the correct cid")
+          const getFile = await contractInstance.getFile.call(_cid)
+          assert.equal(getFile, _cid, "has the correct cid")
         }
         catch(error) {
             console.error(error)
         }
         
     })
+
+    // it("shares files with another user", async() => {
+    //   try {
+    //     const _cid = "another test cid to share files"
+    //     const addFile = await contractInstance.addFile.call(_cid)
+    //     assert.equal(addFile, _cid, "has the correct cid")
+    //     console.log(accounts[1])
+    //     const sharedFile = await contractInstance.shareFile.call(accounts[1], _cid)
+    //     assert.equal(sharedFile, _cid, "has the correct cid")
+    //   }
+    //   catch(error) {
+    //     console.error(error)
+    //   }
+    // })
 })
